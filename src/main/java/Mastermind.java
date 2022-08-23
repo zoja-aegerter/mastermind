c3628188eb5a5abadfec0711932092350605bcb3
Zoja Aegerter <info@aegerter-4u.ch>
2022-08-23 21:20:52 +0200

    Project setup 6 Versuche

--- src/main/java/Mastermind.java
+++ src/main/java/Mastermind.java
@@ -2,6 +2,6 @@ public class Mastermind {
 
     public static void main(String[] args) {
         System.out.println("Geben Sie einen Versuchscode ein mit vier Buchstaben aus der Menge {r, g, b, y, s, w} ein: ");
-        System.out.println("Spiel beendet. Geheimcode war xxxx. Anzahl Versuche : 5");
+        System.out.println("Spiel beendet. Geheimcode war xxxx. Anzahl Versuche : 6");
     }
 }
